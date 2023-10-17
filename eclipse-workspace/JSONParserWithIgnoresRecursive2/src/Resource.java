176e7f4f5997455553f0ccd2d56dd55790c095ec
m-amaris <66205563+m-amaris@users.noreply.github.com>
2023-10-17 23:20:31 +0200

    JSONParser con 1er resource completo

---
+++ eclipse-workspace/JSONParserWithIgnoresRecursive2/src/Resource.java
@@ -0,0 +1,113 @@
+
+public class Resource {
+	private String resourceId;//@id
+	private String conceptId;//@type
+	private String title;
+	private String link; //relation
+	private String eventLocation; //street-address
+	private String area;
+	private String startDate;
+	private String startTime;
+	private String endDate;
+	private String endTime;
+	private double latitude;
+	private double longitude;
+	private String accesibility;
+	private String organizationName;
+	private String description;//services
+	
+	public String getResourceId() {
+		return resourceId;
+	}
+	public void setResourceId(String resourceId) {
+		this.resourceId = resourceId;
+	}
+	public String getConceptId() {
+		return conceptId;
+	}
+	public void setConceptId(String conceptId) {
+		this.conceptId = conceptId;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getEventLocation() {
+		return eventLocation;
+	}
+	public void setEventLocation(String eventLocation) {
+		this.eventLocation = eventLocation;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public String getAccesibility() {
+		return accesibility;
+	}
+	public void setAccesibility(String accesibility) {
+		this.accesibility = accesibility;
+	}
+	public String getOrganizationName() {
+		return organizationName;
+	}
+	public void setOrganizationName(String organizationName) {
+		this.organizationName = organizationName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+	
+}
